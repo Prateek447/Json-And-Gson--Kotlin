dea712ccad6727836ac5ce48c240effbe4f26b9e
Prateek <yprateek51@gmail.com>
2021-05-08 21:10:47 +0530

    Without Gson

---
+++ app/src/main/java/com/example/jokar/json_and_gson/UserModelClass.kt
@@ -0,0 +1,13 @@
+package com.example.jokar.json_and_gson
+
+//Creating a data model class for User
+class UserModelClass(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val weight: Double,
+    val height: Int,
+    val mobile: String,
+    val office: String
+)
